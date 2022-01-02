369b3a7697b636378b5f029416a00cdd59599acf
BeepSterr <git@beeps.dev>
2022-01-03 00:28:50 +0100

    [deploy] add a lot of cool shit

---
+++ src/pages/about_me.js
@@ -0,0 +1,19 @@
+const Block = require("../../lib/block");
+const path = require("path");
+const Sitemap = require("../sitemap.json");
+
+module.exports = {
+
+	template: new Block(path.join(__dirname, '../blocks/templates/dark.html')),
+	data: (self)=>{
+
+		return {
+			title: "About Me",
+			bloglist: new Block(path.join(__dirname, '../blocks/test/bloglist.html')),
+			stamps: new Block(path.join(__dirname, '../blocks/stuff/stampbar.html')),
+			sitemap: Sitemap
+		};
+
+	}
+
+}
