1d4c0d4e66954d82b15305f352ebc580b750d7be
BeepSterr <git@beeps.dev>
2022-05-22 00:55:26 +0200

    [deploy] add steam linker legal shit

---
+++ src/pages/steamlinker/privacy.js
@@ -0,0 +1,15 @@
+const Block = require("../../../lib/block");
+const path = require("path");
+
+module.exports = {
+
+	template: new Block(path.join(__dirname, '../../blocks/templates/dark.html')),
+	data: (self)=>{
+
+		return {
+			title: "Privacy Policy",
+		};
+
+	}
+
+}
